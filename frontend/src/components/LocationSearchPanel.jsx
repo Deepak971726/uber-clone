75164d93245144ccf20d76f705e1d11844b7543f
Deepak <dy705327@gmail.com>
2026-01-16 22:41:12 +0530

    confirm-ride-panel-done

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = ({setPanelOpen ,setVehiclePanel}) => {
+  
+  const locations = [
+    '24B, Near keee coaafee, new delhi technology',
+    '24B, Near keee coaafee, new delhi technology',
+    '24B, Near keee coaafee, new delhi technology'
+  ]
+  
+  const onClickHandler =()=>{
+    setVehiclePanel(true)
+    setPanelOpen(false)
+    // console.log("cldsfdf")
+  }
   return (
     <div>
-      Location serach panel
+      {/* Location serach panel */}
+      {locations.map((item,index)=>{
+        return <div onClick={onClickHandler} key={index} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
+                <h2 className='bg-[#eee] flex items-center justify-center h-8 w-12 rounded-full' ><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-base font-medium'>{item}</h4>
+              </div>
+      })}
+      
+       
+       
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,33 +1,161 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
+import VehiclePanel from '../components/VehiclePanel'
+import ConfirmRide from '../components/ConfirmRide'
+import LookingForDriver from '../components/LookingForDriver'
 
 const Home = () => {
   
+  const [pickup , setPickup]= useState('')
+  const [destination, setDestination] = useState('')
+  const [panelOpen, setPanelOpen] = useState(false)
+  const [vehiclePanel, setVehiclePanel] = useState(false)
+  const [confirmRidePanel,setConfirmRidePanel] = useState(false)
+  const [VehicleFound, setVehicleFound] = useState(null)
+   
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
+  const vehiclePanelRef = useRef(null)
+  const confirmRidePanelRef = useRef(null)
+  const vehicleFoundRef = useRef(null)
+  
+  
+  
   const submitHandler=(e)=>{
     e.preventDefault()
+    console.log(pickup,destination)
   }
+  
+  useGSAP(function(){
+    // console.log(panelOpen)
+    if(panelOpen){
+       gsap.to(panelRef.current,{
+        height:'70%',
+        padding:24
+        // opacity:1
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
+      })
+    }
+    else{
+      gsap.to(panelRef.current,{
+        height:'0%',
+        // opacity:0
+        padding:0
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:0
+      })
+    }
+   
+      
+  },[panelOpen])
+  
+  
+  useGSAP(function(){
+    // console.log("me bhi chal rha hiu")
+    if (vehiclePanel) {
+        gsap.to(vehiclePanelRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(vehiclePanelRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+    
+  },[vehiclePanel])
+  
+  useGSAP(function(){
+    // console.log("me bhi chal rha hiu")
+    if (confirmRidePanel) {
+        gsap.to(confirmRidePanelRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(confirmRidePanelRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+    
+  },[confirmRidePanel])
+  
+  useGSAP(function(){
+    // console.log("me bhi chal rha hiu")
+    if (VehicleFound) {
+        gsap.to(vehicleFoundRef.current, {
+            transform: 'translateY(0)'
+        })
+    } else {
+        gsap.to(vehicleFoundRef.current, {
+            transform: 'translateY(100%)'
+        })
+    }
+    
+  },[VehicleFound])
+  
+  
      
   return (
-    <div className='h-screen relative'>
-      <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"/>
+    <div className='h-screen relative overflow-hidden'>
+      <img  className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"/>
       
       <div className='h-screen w-screen'>
         <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"/>
       </div>
       <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] bg-white  relative p-5'>
-          <h4 className='text-3xl font-semibold'>Find a trip</h4>
-          <form onSubmit={(e)=>submitHandler(e)}>
-            <div className='line absolute h-16 w-1 top-[37%] left-10 bg-gray-700 rounded-full'></div>
-            <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5' type="text" placeholder='Add a pick-up location' />
-            <input className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-3' type="text" placeholder='Enter your destination' />
-          </form>
-        </div>
-        <div className='bg-red-500 h-0'>
+        <div className='h-[30%] bg-white  relative p-6'>
+          <h5 ref={panelCloseRef} className='absolute top-3 right-2 text-2xl'>
+            <i onClick={()=>setPanelOpen(false)} className="ri-arrow-down-wide-line"></i>
+          </h5>
           
+          <h4 className='text-3xl font-semibold'>Find a trip</h4>
+          <form className='relative py-3' onSubmit={(e)=>submitHandler(e)}>
+            <div className='line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-700 rounded-full'></div>
+            <input 
+              onChange={(e)=>setPickup(e.target.value)} 
+              onClick={()=>setPanelOpen(true)}
+              className='bg-[#eee] px-12 py-2 text-base rounded-lg w-full mt-5' 
+              type="text" 
+              value={pickup}
+              placeholder='Add a pick-up location' />
+            <input 
+              onChange={(e)=>setDestination(e.target.value)}
+               onClick={()=>setPanelOpen(true)}
+              value={destination}
+              className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3' 
+              type="text" 
+              placeholder='Enter your destination' />
+          </form>
+          {/* <button
+              // onClick={findTrip}
+              className='bg-black text-white px-4 py-2 rounded-lg mt-3 w-full'>
+              Find Trip
+          </button> */}
+        </div>
+        <div ref={panelRef} className='bg-white h-0'>
+          <LocationSearchPanel setPanelOpen={setPanelOpen} vehiclePanel={vehiclePanel} setVehiclePanel={setVehiclePanel}/>
         </div>
       </div>
       
+      <div ref={vehiclePanelRef} className='fixed w-full bg-white z-10 bottom-0 px-3 py-8 translate-y-full'>
+          <VehiclePanel setConfirmRidePanel={setConfirmRidePanel} vehiclePanel={vehiclePanel} setVehiclePanel={setVehiclePanel}/>
+      </div>
+      <div ref={confirmRidePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-1'>
+        <ConfirmRide setVehicleFound={setVehicleFound} confirmRidePanel={confirmRidePanel} setConfirmRidePanel={setConfirmRidePanel}/>
+      </div>
+      <div ref={vehicleFoundRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-6 pt-1'>
+        <LookingForDriver setVehicleFound={setVehicleFound}/>
+      </div>
+      
     </div>
+    
+    
+    //https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=1152/height=768/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy8xZGRiOGM1Ni0wMjA0LTRjZTQtODFjZS01NmExMWEwN2ZlOTgucG5n
   )
 }
 
